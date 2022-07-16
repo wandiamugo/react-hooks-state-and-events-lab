c149e82d9d3eeb495e9a5e17a05029c340fbb502
Wandia E Mugo <wwandia173@gmail.com>
2022-07-16 12:24:32 +0300

    commit tree 474bcb971e455c88657e8073514c8125a62ac7b7

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React,{useState} from "react";
+
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false)
+
+  function handleEvent() {
+    setIsInCart((isInCart) => !isInCart)
+  }
   return (
-    <li className="">
+    <li className= {isInCart? "in-Cart" : ""}>
+
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleEvent} className= {isInCart? "remove" : "add"}>{isInCart? "Remove from" : "Add to"}Cart</button>
     </li>
   );
 }
